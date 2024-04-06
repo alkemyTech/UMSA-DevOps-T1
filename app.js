9a8895521fdb0f4c047f90814f4c41140c67f7bc
lealivan009 <lealivan009@gmail.com>
2024-04-05 21:44:15 -0300

    Main

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Definir una ruta para el mensaje "Hola Mundo"
 app.get('/', (req, res) => {
-  res.send('MICROSERVICIO DE COMPRAS');
+  res.send('¡Hola Mundo desde un microservicio en Node.js!');
 });
 
 // Iniciar el servidor en el puerto 3000
